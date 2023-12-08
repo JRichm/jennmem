6e30a1f893befbc8c35efd2a4608cff38b42e7f4
James Richmond <jblackenmusic@gmail.com>
2023-12-08 14:18:54 -0700

    updated seed database

--- seedDatabase.py
+++ seedDatabase.py
@@ -5,8 +5,8 @@ from model import connect_to_db, db
 
 
 # delete db to start fresh
-os.system("dropdb -U jamcam jennmem")
-os.system("createdb -U jamcam jennmem")
+os.system("dropdb -U jamcam jennmemdb")
+os.system("createdb -U jamcam jennmemdb")
 
 # use app context
 with app.app_context():
