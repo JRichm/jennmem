b0efb17299641431ba54b0afd93b1bd1ddc83b83
James Richmond <jblackenmusic@gmail.com>
2023-12-08 13:25:32 -0700

    added index route to 5000 server

--- server.py
+++ server.py
@@ -127,6 +127,12 @@ def get_memories():
 
     return jsonify(memories_json)
 
+@app.route('/')
+def index():
+    return jsonify({
+        "Page": "Index"
+    })
+
 if __name__ == '__main__':
     connect_to_db(app)
     app.run(host="0.0.0.0", debug=True)
